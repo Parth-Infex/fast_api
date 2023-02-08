a432b1c57a23424c6fd4f394cb18d9313922af3e
Infex Biztech Private Limited <parth@m.infex.in>
2023-02-08 16:10:01 +0530

    fastapi query parameters

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':'blog list'}
-
+@app.get('/blog')
+def index(limit=10,published : bool=True,sort: Optional[str] = None):
+    if published:
+        return {'data':f'{limit} published blogs from the db'}
+    else:
+        return {'data':f'{limit} blogs from the db'}
 
 @app.get('/blog/unpublished')
 def unpublished():
