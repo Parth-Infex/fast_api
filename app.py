60d8fe8f59b799245bd9b3f66b2a68b5a83ecc3e
Infex Biztech Private Limited <parth@m.infex.in>
2023-02-08 16:31:21 +0530

    fastapi in request body

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -25,3 +26,12 @@ def show(id: int):
 def comments(id):
     # fetch comments of blog with id = id
     return {'data': {'1','2'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data':f'Blog is created with title as {request.title}'}
